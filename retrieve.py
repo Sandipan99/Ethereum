1248ea1a116678a41087c967251035f9730d8844
Sandipan Sikdar <sandipansikdar@ht-167.humtec.rwth-aachen.de>
2018-05-09 12:21:18 +0200

    retrieve

--- retrieve.py
+++ retrieve.py
@@ -1,36 +1,59 @@
+# Implements several functions for retrieving information from blocks
+# Function names are self-explanatory
+
 import json
+
+__author__ = 'Sandipan Sikdar'
 
 def retrieveBlock(block_num):
 	b_str = str(block_num)
 	path = 'blocks/'
-	## finding the file path for the block.....3 levels of hashing 
+	## finding the file path for the block.....3 levels of hashing
 	if len(b_str)>=3:
 		path += b_str[0]+"/"+b_str[1]+"/"+b_str[2]+"/"
 	elif len(b_str)==2:
 		path += b_str[0]+"/"+b_str[1]+"/"
-	else:	
+	else:
 		path += b_str[0]+"/"
 
 	file_name = path+"blocks"
-	
+
 	with open(file_name) as fs:
 		for line in fs:
-			data = json.loads(line.strip())	
+			data = json.loads(line.strip())
 			if data['number'] == block_num:
 				return data
-	return None			
+	return None
 
 
 def displayBlock(block_num):
 	data = retrieveBlock(block_num)
-	for key in data:
-		print(key+": "+str(data[key]))
+	if data==None:
+		print("block not found")
+	else:
+		for key in data:
+			print(key+": "+str(data[key]))
 
 #def retrieve_account_transaction(account,timestamp):
 
 
 def getTransactions(block_num):
-	data = 
+	data = retrieveBlock(block_num)
+	if data==None:
+		return None
+	elif data['transaction count'] == 0:
+		return None
+	else:
+		return data['transactions']
+
+def displayTransactions(block_num):
+	trans = getTransactions(block_num)
+	if trans==None:
+		print("No transactions found for this block")
+	else:
+		for i in range(len(trans)):
+			for key in trans[i]:
+				print(key+": "+str(trans[i][key]))	
 
 if __name__=="__main__":
-	retrieveBlock(409912)					
+	retrieveBlock(409912)
